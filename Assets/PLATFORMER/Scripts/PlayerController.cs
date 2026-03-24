28f9c29edf5f2560ebd798a5987792e0444dd8c0
Lanzall <benrock111e@gmail.com>
2026-03-24 18:56:59 +1100

    platformer stuff

---
+++ Assets/PLATFORMER/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private PlatformerPlayer controls;
+    private InputAction moveInput;
+    private InputAction jumpInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        //Horizontal
+
+    }
+}
